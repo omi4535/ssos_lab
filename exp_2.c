e3e53746fdb11e7550f073d600d75218e7ebcb15
omi4535 <87719359+omi4535@users.noreply.github.com>
2022-02-23 23:03:06 +0530

    Add files via upload

---
+++ exp_2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+char arr[50];
+int i, vovel = 0, consonent = 0, digit = 0, space = 0, acout = 0, ecout = 0, icout = 0, ocout = 0, ucout = 0;
+
+int main()
+{
+
+    printf("\nenter the string");
+
+    fgets(arr, sizeof(arr), stdin);
+
+    printf("\nyou enter the string\n");
+    puts(arr);
+
+    for (i = 0; arr[i] != '\0'; ++i)
+    {
+        arr[i] = tolower(arr[i]);
+
+        if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' ||
+            arr[i] == 'o' || arr[i] == 'u')
+        {
+            if (arr[i] == 'a')
+            {
+                ++acout;
+            }
+
+            else if (arr[i] == 'e')
+            {
+                ++ecout;
+            }
+
+            else if (arr[i] == 'i')
+            {
+                ++icout;
+            }
+
+            else if (arr[i] == 'o')
+            {
+                ++ocout;
+            }
+
+            else if (arr[i] == 'u')
+            {
+                ++ucout;
+            }
+
+            ++vovel;
+        }
+
+        else if ((arr[i] >= 'a' && arr[i] <= 'z'))
+        {
+            ++consonent;
+        }
+
+        else if (arr[i] >= '0' && arr[i] <= '9')
+        {
+            ++digit;
+        }
+
+        else if (arr[i] == ' ')
+        {
+            ++space;
+        }
+    }
+
+    printf("Vowels: %d", vovel);
+    printf("\nConsonants: %d", consonent);
+    printf("\nDigits: %d", digit);
+    printf("\nWhite spaces: %d", space);
+    printf("\n count of a: %d", acout);
+    printf("\n count of e: %d", ecout);
+    printf("\n count of i: %d", icout);
+    printf("\n count of o: %d", ocout);
+    printf("\n count of u: %d", ucout);
+    return 0;
+}
